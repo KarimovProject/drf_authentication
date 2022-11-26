13decdb15ced5f7a3b5889c719e0cca05d632b3b
ZARIFMAC <uzbzarif@gmail.com>
2022-11-26 18:10:11 +0500

    auth

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,16 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from rest_framework.urlpatterns import format_suffix_patterns
 from api.views import StudentView
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('students/', StudentView.as_view()),
+    
+    path('api/', include('api.urls')),   
+    path('auth/', include('rest_framework.urls')),
 ]
 
 urlpatterns=format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
