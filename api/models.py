6d236bffa80a72b23da0103fb060d87918575f77
ZARIFMAC <uzbzarif@gmail.com>
2022-11-26 17:31:53 +0500

    add model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)    
+    city = models.CharField(max_length=100)
+
+   
+    def __str__(self):
+        return self.name
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    students = models.ManyToManyField(Student, related_name='groups')
+
+    def __str__(self):
+        return self.name
