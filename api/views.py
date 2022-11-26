fde5d610d9f203a9f6ee82f6a6226cb3e1b34541
ZARIFMAC <uzbzarif@gmail.com>
2022-11-26 17:41:33 +0500

    get student

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+# Import the models
+from .models import Student, Group
+# Import the serializers
+from .serializers import StudentSerializer, GroupSerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+def get_students(request: Request):
+    """
+    Get all students
+    """
+    students = Student.objects.all()
+    serializer = StudentSerializer(students, many=True)
+    return Response(serializer.data)
+    
+
